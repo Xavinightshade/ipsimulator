96fb85f9fcb0f20672095d6916d7a7201f9058e5
mig.ruiz <mig.ruiz@8dc7f395-6e37-0410-beab-fd73ee9c2fbb>
2008-08-20 03:09:26 +0000

    clickonce deployment

--- SimuladorRedesIP/Cliente/Properties/AssemblyInfo.cs
+++ SimuladorRedesIP/Cliente/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cliente")]
+[assembly: AssemblyTitle("Simulador TCP/IP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cliente")]
+[assembly: AssemblyProduct("Simulador TCP/IP")]
 [assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
