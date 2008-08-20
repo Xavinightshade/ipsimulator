6f012829328f9ca69aabf1ec0661940179495859
mig.ruiz <mig.ruiz@8dc7f395-6e37-0410-beab-fd73ee9c2fbb>
2008-08-20 14:10:12 +0000

    no se q paso

--- SimuladorRedesIP/SOA/ICallBackContract.cs
+++ SimuladorRedesIP/SOA/ICallBackContract.cs
@@ -9,13 +9,19 @@ namespace RedesIP.SOA
 	public interface IVisualizacion
 	{
 		[OperationContract(IsOneWay = true)]
-		void CrearEquipo(EquipoSOA equipo);
+		void CrearComputador(ComputadorSOA pc);
+        [OperationContract(IsOneWay = true)]
+        void CrearSwitch(SwitchSOA swi);
+        [OperationContract(IsOneWay = true)]
+        void CrearRouter(RouterSOA router);
 		[OperationContract(IsOneWay = true)]
 		void MoverEquipo(Guid idEquipo, int x, int y);
 		[OperationContract(IsOneWay = true)]
 		void ConectarPuertos(CableSOA cable);
-		[OperationContract(IsOneWay = true)]
-		void ActualizarEstacion(List<EquipoSOA> equipos, List<CableSOA> cables);
+        [OperationContract(IsOneWay = true)]
+        void EstablecerDireccionIP(Guid idPuerto, string ipAddress);
+        [OperationContract(IsOneWay = true)]
+		void ActualizarEstacion(EstacionSOA estacion);
 		[OperationContract(IsOneWay = true)]
 		void EnviarInformacionConexion(MensajeSOA mensaje);
 	}
