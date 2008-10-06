6fd487dcc619cb3fb70a56e53602c1931d5f698e
mig.ruiz <mig.ruiz@8dc7f395-6e37-0410-beab-fd73ee9c2fbb>
2008-10-06 15:04:10 +0000

    remoting perfecto

--- SimuladorRedesIP/SOA/Componentes/RutaSOA.cs
+++ SimuladorRedesIP/SOA/Componentes/RutaSOA.cs
@@ -2,33 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace SOA.Componentes
 {
+    [DataContract]
     public class RutaSOA
     {
                 private Guid _id;
 
+                [DataMember]
         public Guid Id
         {
             get { return _id; }
+            set { _id = value; }
         }
         private string _red;
-
+        [DataMember]
         public string Red
         {
             get { return _red; }
             set { _red = value; }
         }
         private Guid _idPuerto;
-
+        [DataMember]
         public Guid IdPuerto
         {
             get { return _idPuerto; }
             set { _idPuerto = value; }
         }
         private string _nombrePuerto;
-
+        [DataMember]
         public string NombrePuerto
         {
             get { return _nombrePuerto; }
@@ -39,7 +43,7 @@ namespace SOA.Componentes
             _id = id;
         }
         private int? _mask;
-
+        [DataMember]
         public int? Mask
         {
             get { return _mask; }
@@ -47,7 +51,7 @@ namespace SOA.Componentes
         }
 
         private string _nextHopIP;
-
+        [DataMember]
         public string NextHopIP
         {
             get { return _nextHopIP; }
